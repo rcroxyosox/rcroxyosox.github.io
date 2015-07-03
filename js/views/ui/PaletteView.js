b64874f4d08154c4a673bb033f96f94a29ac57e8
Rob <robert_cox@intuit.com>
2015-07-03 10:10:28 -0700

    Text input as a response type

--- js/views/ui/PaletteView.js
+++ js/views/ui/PaletteView.js
@@ -16,7 +16,7 @@ define([
 			className: 'PaletteView',
 			events   : {},
 			template: Handlebars.compile(html),
-			colors: [],
+			colors: [{"name":"$gray1","color":"#eff2ec"},{"name":"$gray2","color":"#eeeded"},{"name":"$gray2_3","color":"#e3e3e3"},{"name":"$gray3","color":"#999999"},{"name":"$gray6","color":"#333"},{"name":"$purple2","color":"#6563a4"},{"name":"$purple3","color":"#5f5d9a"},{"name":"$purple4","color":"#7a4364"},{"name":"$pink2","color":"#d667cd"},{"name":"$green2","color":"#6dd5c9"},{"name":"$blue3","color":"#00bcd4"}],
 			_base: null,
 			initialize: function(){
 				var that = this;
